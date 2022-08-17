6e41962bd4642dc3207290608a8e684a785bf2bd
skyun <skyun@a2tec.co.kr>
2022-08-17 23:51:26 +0900

    Init spring boot project

---
+++ src/test/java/me/kenux/travelog/TravelogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kenux.travelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
