4a1fb417a6f9e961b70006c1f231459410488b57
skyun <kenux.yun@gmail.com>
2022-11-03 13:58:37 +0900

    Modify test

--- src/test/java/me/kenux/travelog/TravelHistoryApplicationTests.java
+++ src/test/java/me/kenux/travelog/TravelHistoryApplicationTests.java
@@ -3,7 +3,7 @@ package me.kenux.travelog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TravelHistoryApplicationTests {
 
 	@Test
