c6617a01b8e74e477edddcfba9b2ccf82528e76f
skyun <kenux.yun@gmail.com>
2022-11-08 16:55:33 +0900

    H2 MySQL 호환모드 설정

--- src/test/java/me/kenux/travelog/repository/TravelHistoryCommentRepositoryTest.java
+++ src/test/java/me/kenux/travelog/repository/TravelHistoryCommentRepositoryTest.java
@@ -3,11 +3,13 @@ package me.kenux.travelog.repository;
 import me.kenux.travelog.domain.TravelHistoryComment;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-@ActiveProfiles("local")
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TravelHistoryCommentRepositoryTest {
 
     @Autowired
--- src/test/java/me/kenux/travelog/repository/TravelHistoryRepositoryTest.java
+++ src/test/java/me/kenux/travelog/repository/TravelHistoryRepositoryTest.java
@@ -6,6 +6,7 @@ import me.kenux.travelog.domain.enums.TravelType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -16,9 +17,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 //@Rollback(value = false)
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TravelHistoryRepositoryTest {
 
     @Autowired
