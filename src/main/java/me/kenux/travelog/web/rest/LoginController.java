01351a36f8b32101ae529f31434e7b307297a752
skyun <kenux.yun@gmail.com>
2022-11-10 19:40:14 +0900

    CROS 수정

---
+++ src/main/java/me/kenux/travelog/web/rest/LoginController.java
@@ -0,0 +1,16 @@
+package me.kenux.travelog.web.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+public class LoginController {
+
+    @PostMapping
+    public ResponseEntity<?> login() {
+        return ResponseEntity.ok().build();
+    }
+}
