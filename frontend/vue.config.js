a3dfb94892a83c18a22292dadec4d7feeacb38a5
skyun <kenux.yun@gmail.com>
2022-11-10 18:30:51 +0900

    [frontend] Login vue

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,11 +8,11 @@ module.exports = {
   indexPath: "../templates/index.html",
   devServer: {
     port: 3000,
-    proxy: "http://localhost:8080"
+    proxy: "http://localhost:8080",
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     const svgRule = config.module.rule("svg");
     svgRule.uses.clear();
     svgRule.use("vue-svg-loader").loader("vue-svg-loader");
-  }
-}
+  },
+};
