1c163db96600f03166f4cbdf2b24209f363e5231
skyun <kenux.yun@gmail.com>
2022-11-10 19:16:33 +0900

    [frontend] eslint 설정 및 bootstrap 추가

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="about">
-        <h1>This is an about page ^^ kenux</h1>
-        <Login msg="여기는 kenux의 개인 프로젝트 화면입니다." />
-    </div>
+  <div class="about">
+    <h1>This is an about page ^^ kenux</h1>
+    <Login msg="여기는 kenux의 개인 프로젝트 화면입니다." />
+  </div>
 </template>
 <script>
-import Login from "@/components/Login.vue";
+import Login from '@/components/Login.vue';
 
 export default {
-    name: "AboutView",
-    components: {
-        Login,
-    },
+  name: 'AboutView',
+  components: {
+    Login,
+  },
 };
 </script>
