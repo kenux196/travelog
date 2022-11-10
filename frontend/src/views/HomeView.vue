90bc6f7dccb54f6c3b28b2437b996a10252bbf0d
skyun <kenux.yun@gmail.com>
2022-11-10 19:16:33 +0900

    [frontend] eslint 설정 및 bootstrap 추가

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="여기는 kenux의 개인 프로젝트 화면입니다." />
-    </div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="여기는 kenux의 개인 프로젝트 화면입니다." />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-    name: "HomeView",
-    components: {
-        HelloWorld,
-    },
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
 };
 </script>
