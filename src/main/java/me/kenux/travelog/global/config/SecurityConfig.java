d015509b3be254b1df97718f9b093d0c8c1599af
skyun <kenux.yun@gmail.com>
2022-11-14 15:08:39 +0900

    [Thymeleaf] 테스트

--- src/main/java/me/kenux/travelog/global/config/SecurityConfig.java
+++ src/main/java/me/kenux/travelog/global/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //            .and()
             .authorizeRequests()
             .antMatchers("/").permitAll()
+            .antMatchers("/thymeleaf/*").permitAll()
             .anyRequest().authenticated()
             .and()
 //            .httpBasic()
---
+++ src/main/java/me/kenux/travelog/web/web/ThymeleafTestController.java
@@ -0,0 +1,18 @@
+package me.kenux.travelog.web.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafTestController {
+
+    @GetMapping("/main")
+    public String getThymeleafMain(Model model) {
+        model.addAttribute("data", "Hello Spring!");
+        model.addAttribute("data1", "Hello <b>Spring!<b>");
+        return "thymeleaf/main";
+    }
+}
