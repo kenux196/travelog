4ef6092b2aa9aca2fd2ec307b99a8f960f04b097
skyun <kenux.yun@gmail.com>
2022-11-15 17:45:50 +0900

    [security] 정적 리소스 권한 허용 처리 변경

--- src/main/java/me/kenux/travelog/global/config/SecurityConfig.java
+++ src/main/java/me/kenux/travelog/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package me.kenux.travelog.global.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void configure(WebSecurity web) {
-        web
-            .ignoring() // spring security 필터 타지 않도록 설정
-            .antMatchers("/resources/**") // 정적 리소스 무시
-            .antMatchers("/h2-console/**"); // h2-console 무시
-    }
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web
+//            .ignoring() // spring security 필터 타지 않도록 설정
+//            .antMatchers("/h2-console/**"); // h2-console 무시
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,6 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //            .and()
             .authorizeRequests()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            .antMatchers("/h2-console/**").permitAll()
             .antMatchers("/").permitAll()
             .antMatchers("/thymeleaf/**").permitAll()
             .anyRequest().authenticated()
