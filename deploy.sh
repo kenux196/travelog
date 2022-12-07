704e0c4c40180c53259267cc9e049f51d94073f3
skyun <skyun@a2tec.co.kr>
2022-12-07 21:28:00 +0900

    Add web-app-runner.sh file

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Gradle Build..."
 echo "Copy jar file..."
 cp build/libs/travelog-0.0.1-SNAPSHOT.jar ./app.jar
 echo "오라클 인스턴스로 업로드 시작"
-scp ./app.jar ubuntu@kenux.duckdns.org:/home/ubuntu/web
+scp ./app.jar web-app-runner.sh ubuntu@kenux.duckdns.org:/home/ubuntu/web
 rm app.jar
 echo "배포 완료. 서버에 접속해서 앱 재실행하세요..."
---
+++ web-app-runner.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+processId=$(lsof -t -i:8080)
+echo "Kill web app process $processId"
+kill -TERM "$processId"
+
+echo "Waiting 3 seconds."
+sleep 3
+
+active='local'
+if [ -n "$1" ]
+then
+    active=$1
+fi
+echo "Run web app => active: $active"
+nohup java -jar app.jar --spring.profiles.active="$active" &
