1e189da8d8c7ac056d23b5596c1129b1a842870c
skyun <kenux.yun@gmail.com>
2022-12-13 17:44:03 +0900

    관리자 회원 삭제 API 추가 : 내용 구현 필요

--- src/main/java/me/kenux/travelog/web/web/admin/MemberManageAdminController.java
+++ src/main/java/me/kenux/travelog/web/web/admin/MemberManageAdminController.java
@@ -1,23 +1,31 @@
-package me.kenux.travelog.web.web;
+package me.kenux.travelog.web.web.admin;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.kenux.travelog.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/admin/members")
 @RequiredArgsConstructor
-public class AdminController {
+@Slf4j
+public class MemberManageAdminController {
 
     private final MemberService memberService;
 
-    @GetMapping("/members")
+    @GetMapping
     public String getMembers(Model model) {
         model.addAttribute("memberInfo", memberService.getMemberInfoResponse());
         return "view/admin/member-list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteMember(@RequestParam("id") Long id) {
+        log.info("will delete member id = {}", id);
+        return "redirect:/admin/members";
     }
 }
