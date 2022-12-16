2036ca7f024c5742519a569b3359adcc0b278a2f
kenux196 <kenux.yun@gmail.com>
2022-12-17 01:46:50 +0900

    fix test failed

--- src/test/java/me/kenux/travelog/domain/member/repository/MemberBaseRepositoryTest.java
+++ src/test/java/me/kenux/travelog/domain/member/repository/MemberBaseRepositoryTest.java
@@ -35,22 +35,6 @@ class MemberBaseRepositoryTest extends BaseRepositoryTest {
         assertThat(findMember).isPresent();
     }
 
-    @Test
-    void saved_status_value_test() {
-        // given
-        final Member newMember = getMember();
-        newMember.leave();
-
-        // when
-        memberRepository.save(newMember);
-        em.clear();
-
-        // then
-        final Optional<Member> findMember = memberRepository.findById(newMember.getId());
-        assertThat(findMember).isPresent();
-        assertThat(findMember.get().getStatus()).isEqualTo(MemberStatus.LEAVED);
-    }
-
     private static Member getMember() {
         final UserPassword password = new UserPassword("password");
         return Member.createNewMember("member1", "member1@email.com", password);
