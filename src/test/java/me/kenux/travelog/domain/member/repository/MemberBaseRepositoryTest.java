1671643299f49ca48b35850b49713d8a0438dfce
kenux196 <kenux.yun@gmail.com>
2022-12-17 18:13:52 +0900

    Fix test failed

--- src/test/java/me/kenux/travelog/domain/member/repository/MemberBaseRepositoryTest.java
+++ src/test/java/me/kenux/travelog/domain/member/repository/MemberBaseRepositoryTest.java
@@ -6,6 +6,7 @@ import me.kenux.travelog.domain.member.entity.enums.MemberStatus;
 import me.kenux.travelog.base.BaseRepositoryTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.persistence.EntityManager;
 
@@ -20,11 +21,16 @@ class MemberBaseRepositoryTest extends BaseRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    PasswordRepository passwordRepository;
 
     @Test
     void save() {
         // given
-        final Member newMember = getMember();
+        final UserPassword password = new UserPassword("password");
+        passwordRepository.save(password);
+        final Member newMember = Member.createNewMember("member1", "member1@email.com", password);;
+
 
         // when
         memberRepository.save(newMember);
@@ -33,10 +39,5 @@ class MemberBaseRepositoryTest extends BaseRepositoryTest {
         // then
         final Optional<Member> findMember = memberRepository.findById(newMember.getId());
         assertThat(findMember).isPresent();
-    }
-
-    private static Member getMember() {
-        final UserPassword password = new UserPassword("password");
-        return Member.createNewMember("member1", "member1@email.com", password);
     }
 }
