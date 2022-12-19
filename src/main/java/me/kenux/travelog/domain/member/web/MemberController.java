44a4f0b8da43d9b24ba6101dbb5357e9eee2b895
kenux196 <kenux.yun@gmail.com>
2022-12-19 17:44:59 +0900

    Remove not need controller api

--- src/main/java/me/kenux/travelog/domain/member/web/MemberController.java
+++ src/main/java/me/kenux/travelog/domain/member/web/MemberController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import me.kenux.travelog.domain.member.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
@@ -12,12 +14,6 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
-
-    @GetMapping
-    public String getMembers(Model model) {
-        model.addAttribute("memberInfo", memberService.getMemberInfoResponse());
-        return "view/admin/member-list";
-    }
 
     @GetMapping("/{id}")
     public String getMemberDetail(@PathVariable("id") Long id, Model model) {
