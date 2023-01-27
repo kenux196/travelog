6d169fe9a35b1cd366f7c6ed8cb01643d6eb1bf6
skyun <kenux.yun@gmail.com>
2023-01-27 17:39:53 +0900

    spring security: h2console error 수정

--- src/main/java/me/kenux/travelog/global/config/SecurityConfig.java
+++ src/main/java/me/kenux/travelog/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import me.kenux.travelog.global.security.jwt.JwtTokenProvider;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -29,7 +30,15 @@ public class SecurityConfig {
     private final UserDetailsService userDetailsService;
 
     @Bean
+    @Profile("!local")
     public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    @Profile("local")
+    public WebSecurityCustomizer webSecurityCustomizerLocal() {
         return web -> web.ignoring()
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
             .requestMatchers(toH2Console());
