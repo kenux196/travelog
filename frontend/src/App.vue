d5c57a6df0eef6d616260d3e8c3e09c0c6d7bf37
skyun <kenux.yun@gmail.com>
2023-02-03 18:00:37 +0900

    feat: frontend logout button show/hide

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="nav-area">
-    <b-navbar type="light" variant="light">
-      <b-navbar-brand tag="h1" class="mb-0">My TEST</b-navbar-brand>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="/login">Login</b-nav-item>
-          <b-nav-item to="/about">About</b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item to="/admin">관리자-메인</b-nav-item>
-          <b-nav-item to="/admin/member">관리자-회원관리</b-nav-item>
-          <b-nav-item @click="logout">Logout</b-nav-item>
-          <b-nav-item to="/vue-test">Vue.js Test</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+  <div class="container">
+    <div class="nav-area">
+      <b-navbar type="light" variant="light">
+        <b-navbar-brand tag="h1" class="mb-0">My TEST</b-navbar-brand>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/login">Login</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item to="/admin">관리자-메인</b-nav-item>
+            <b-nav-item to="/admin/member">관리자-회원관리</b-nav-item>
+            <b-nav-item v-if="isLoggedIn()" @click="logout">Logout</b-nav-item>
+            <b-nav-item to="/vue-test">Vue.js Test</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
 <script>
+import store from '@/store/store';
 export default {
   data() {
     return {};
@@ -28,17 +31,24 @@ export default {
     logout() {
       alert('logout api 호출 연결');
     },
+    isLoggedIn() {
+      return store.getters.isLogin ? true : false;
+    },
   },
 };
 </script>
 
 <style>
-body {
-  margin: 10%;
-}
 #app {
   width: 100vw;
   height: 100vh;
   text-align: center;
+}
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
