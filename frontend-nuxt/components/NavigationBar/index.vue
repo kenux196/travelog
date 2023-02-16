dd828dabffea4d0ef3195e10b6afe49ddf966144
skyun <kenux.yun@gmail.com>
2023-02-16 19:15:21 +0900

    nuxt: add admin page

--- frontend-nuxt/components/NavigationBar/index.vue
+++ frontend-nuxt/components/NavigationBar/index.vue
@@ -1,9 +1,34 @@
 <template>
   <nav>
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/test">test</NuxtLink>
-    <NuxtLink to="/test2">test2</NuxtLink>
-    <NuxtLink to="/about">about</NuxtLink>
-    <NuxtLink to="/login">Login</NuxtLink>
-  </nav>
+      <ul>
+        <li><strong>KENUX JAMIRO</strong></li>
+      </ul>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/test">test</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/test2">test2</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/vue-test">Vue.js Test</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/admin">관리자-메인</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/admin/member">관리자-회원관리</NuxtLink>
+        </li>
+        <li>
+          <a href="#">Logout</a>
+          <NuxtLink>Login</NuxtLink>
+        </li>
+      </ul>
+    </nav>
 </template>
