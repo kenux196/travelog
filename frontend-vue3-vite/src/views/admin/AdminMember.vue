fb649f91a8bba8da37bd25bd35af4cf0697bca21
kenux196 <kenux.yun@gmail.com>
2023-03-02 16:50:12 +0900

    new vue : 회원 리스트 화면 수정

--- frontend-vue3-vite/src/views/admin/AdminMember.vue
+++ frontend-vue3-vite/src/views/admin/AdminMember.vue
@@ -2,17 +2,21 @@
   <h1>Admin 회원 관리 페이지</h1>
   <table>
     <thead>
+      <th>#</th>
       <th>이름</th>
       <th>이메일</th>
       <th>role</th>
+      <th>상태</th>
       <th>등록일</th>
     </thead>
     <tbody>
       <tr v-for="member in members" :key="member.id">
+        <td>{{ member.id }}</td>
         <td>{{ member.name }}</td>
         <td>{{ member.email }}</td>
         <td>{{ member.role }}</td>
-        <td>{{ member.createDate }}</td>
+        <td>{{ member.status }}</td>
+        <td>{{ displayDate(member.joinDate) }}</td>
       </tr>
     </tbody>
   </table>
@@ -40,4 +44,9 @@ async function getMembers() {
 onMounted(() => {
   getMembers();
 });
+
+function displayDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleString();
+}
 </script>
