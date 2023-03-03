7dd24883106bacdfa75c2356d34d2d880d2d18e9
kenux196 <kenux.yun@gmail.com>
2023-03-03 19:55:47 +0900

    login refactoring

--- frontend-vue3-vite/src/views/LoginView.vue
+++ frontend-vue3-vite/src/views/LoginView.vue
@@ -3,8 +3,8 @@
   <div class="login">
     <input type="text" placeholder="email" v-model="user" />
     <input type="password" v-model="password" />
-    <button type="submit" @click="login()">로그인</button>
-    <p v-if="error" class="error">Bad login information.</p>
+    <button type="submit" @click="requestLogin()">로그인</button>
+    <p v-if="loginError" class="error">Bad login information.</p>
   </div>
   <div class="protected" v-if="loginSuccess">
     <h5>{{ store.role }} 로그인 성공!</h5>
@@ -18,46 +18,53 @@
 </template>
 
 <script setup>
-import axios from 'axios';
+// import axios from 'axios';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '../stores/auth';
+import { login } from '../common/auth';
 
 const loginSuccess = ref(false);
 const loginError = ref(false);
 const user = ref('');
 const password = ref('');
-const error = ref(false);
+// const error = ref(false);
 
 const router = useRouter();
 const store = useAuthStore();
 
-async function login() {
-  axios
-    .post('/api/auth/login', {
-      username: user.value,
-      password: password.value,
-    })
-    .then((response) => {
-      if (response.status === 200) {
-        console.log(response.data);
-        console.log('accessToken: ' + response.data.accessToken);
-        store.accessToken = response.data.accessToken;
-        store.refreshToken = response.data.refreshToken;
-        store.role = response.data.role;
-        loginSuccess.value = true;
-        loginError.value = false;
-        error.value = false;
-        moveToHome();
-      } else {
-        loginSuccess.value = false;
-        loginError.value = true;
-        error.value = true;
-      }
-    })
-    .catch((e) => {
-      console.error('error : ', e);
-    });
+async function requestLogin() {
+  const loginSuccess = await login(user.value, password.value);
+  if (loginSuccess) {
+    moveToHome();
+  } else {
+    loginError.value = true;
+  }
+  // axios
+  //   .post('/api/auth/login', {
+  //     username: user.value,
+  //     password: password.value,
+  //   })
+  //   .then((response) => {
+  //     if (response.status === 200) {
+  //       console.log(response.data);
+  //       console.log('accessToken: ' + response.data.accessToken);
+  //       store.accessToken = response.data.accessToken;
+  //       store.refreshToken = response.data.refreshToken;
+  //       store.role = response.data.role;
+  //       loginSuccess.value = true;
+  //       loginError.value = false;
+  //       error.value = false;
+  //       moveToHome();
+  //     } else {
+  //       loginSuccess.value = false;
+  //       loginError.value = true;
+  //       error.value = true;
+  //     }
+  //   })
+  //   .catch((e) => {
+  //     console.error('error : ', e);
+  //   });
 }
 
 function moveToHome() {
