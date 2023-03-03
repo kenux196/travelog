4a77a01fc6a97a3fd624cdf9912ad9e95a0f2024
kenux196 <kenux.yun@gmail.com>
2023-03-03 13:32:14 +0900

    vite proxy api 설정 변경:

--- frontend-vue3-vite/vite.config.js
+++ frontend-vue3-vite/vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      '/api/*': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
