14e69c4da137850570f6e0b851b850d155c9113b
kenux196 <kenux.yun@gmail.com>
2023-03-05 00:12:57 +0900

    Modify refresh token error

--- frontend-vue3-vite/src/views/LoginView.vue
+++ frontend-vue3-vite/src/views/LoginView.vue
@@ -1,8 +1,8 @@
 <template>
   <h1>로그인 화면</h1>
   <div class="login">
-    <input type="text" placeholder="email" v-model="user" />
-    <input type="password" v-model="password" />
+    <input type="text" placeholder="이메일 계정을 입력하세요" v-model="user" />
+    <input type="password" placeholder="암호를 입력하세요" v-model="password" />
     <button type="submit" @click="login">로그인</button>
   </div>
 </template>
@@ -15,7 +15,11 @@ const user = ref('');
 const password = ref('');
 
 const login = () => {
-  auth.login(user.value, password.value);
+  if (user.value !== '' && password.value !== '') {
+    auth.login(user.value, password.value);
+    return;
+  }
+  alert('사용자 계정 및 암호를 입력하세요');
 };
 </script>
 
