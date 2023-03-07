82d189f38a1367d74c68cf94ae4814abd89b9842
kenux196 <kenux.yun@gmail.com>
2023-03-07 09:47:23 +0900

    Add BookControllerTest

---
+++ src/test/java/me/kenux/travelog/web/api/book/BookControllerTest.java
@@ -0,0 +1,41 @@
+package me.kenux.travelog.web.api.book;
+
+import me.kenux.travelog.global.security.jwt.JwtTokenProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(BookController.class)
+class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @Test
+    @DisplayName("Book List 가져온다.")
+    @WithMockUser(roles = "USER", username = "user@test.com")
+    void getBooks_success() throws Exception {
+
+        mockMvc.perform(get("/api/books")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$..['title']").exists())
+                .andDo(print());
+    }
+}
