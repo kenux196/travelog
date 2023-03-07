42720127718850661767218dbbe2719bdea6bc06
kenux196 <kenux.yun@gmail.com>
2023-03-07 16:27:15 +0900

    Remove messagesource & refreshtoken test

---
+++ src/test/java/me/kenux/travelog/domain/member/entity/RefreshTokenEntityTest.java
@@ -0,0 +1,44 @@
+package me.kenux.travelog.domain.member.entity;
+
+import me.kenux.travelog.domain.member.entity.enums.UserRole;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RefreshTokenEntityTest {
+
+    @Test
+    void createRefreshTokenEntity() {
+        // given
+        Member member = Member.builder()
+                .name("user")
+                .email("user@test.com")
+                .userRole(UserRole.USER)
+                .build();
+
+        // when
+        final RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity(member);
+
+        // then
+        assertThat(refreshTokenEntity).isNotNull();
+    }
+
+    @Test
+    void updateRefreshToken() {
+        Member member = Member.builder()
+                .name("user")
+                .email("user@test.com")
+                .userRole(UserRole.USER)
+                .build();
+        final RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity(member);
+
+        // when
+        refreshTokenEntity.updateToken("token");
+
+        // then
+        assertThat(refreshTokenEntity.getToken()).isEqualTo("token");
+    }
+
+}
