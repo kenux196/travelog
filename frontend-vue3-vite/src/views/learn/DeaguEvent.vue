00d2ea0caa28e39a0e861988e222596f0e187cef
kenux196 <kenux.yun@gmail.com>
2023-03-14 17:59:49 +0900

    modify: 검색 조건 수정

--- frontend-vue3-vite/src/views/learn/DeaguEvent.vue
+++ frontend-vue3-vite/src/views/learn/DeaguEvent.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>책 검색</h1>
-  <input type="text" v-model="keyword" />
-  <button @click="searchEvent">검색</button>
+  <h1>대구시 문화/공연/전시 이벤트 정보 검색</h1>
+  <div class="grid">
+    <input type="date" v-model="startDate" />
+    <input type="date" v-model="endDate" />
+    <button @click="searchEvent">검색</button>
+  </div>
   <div>
     <div v-if="hasEvent">
       <h4>검색 결과</h4>
@@ -9,16 +12,14 @@
       <table>
         <thead>
           <th>종류</th>
-          <th>제목</th>
+          <th>주제</th>
           <th>장소</th>
-          <th>가격</th>
+          <th>유료/무료</th>
           <th>기간</th>
         </thead>
         <tbody>
           <tr v-for="(event, index) in eventList" :key="index">
-            <td>
-              {{ event.event_gubun_name }}
-            </td>
+            <td>{{ event.event_gubun_name }}</td>
             <td>{{ event.subject }}</td>
             <td>{{ event.place }}</td>
             <td>{{ event.pay_gubun_name }}</td>
@@ -35,27 +36,27 @@
 import axios from 'axios';
 import { computed, ref } from 'vue';
 
-const keyword = ref('');
+const startDate = ref('');
+const endDate = ref('');
 const eventList = ref('');
 
 const hasEvent = computed(() => {
-  console.log('book list = ' + eventList.value.length);
+  console.log('event list = ' + eventList.value.length);
   return eventList.value.length > 0 ? true : false;
 });
 
 const searchEvent = () => {
-  if (keyword.value.length <= 0) {
-    alert('검색할 책 정보를 입력하세요.');
+  if (startDate.value.length <= 0) {
+    alert('시작일을 입력하세요. 시작일은 필수입니다.');
     return;
   }
-  console.log(keyword.value + ' 책을 검색합니다.');
 
   axios
     .get('/ajax/event/list.json', {
       params: {
         event_gubun: 'DP',
-        start_date: '2023-03',
-        end_date: '2023-04',
+        start_date: startDate.value.toString(),
+        end_date: endDate.value.toString(),
       },
     })
     .then((response) => {
