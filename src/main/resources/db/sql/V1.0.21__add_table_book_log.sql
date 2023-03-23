90a6b9ca04d5e8be1ceee9466cff050a31d91bd5
kenux196 <kenux.yun@gmail.com>
2023-03-23 16:23:38 +0900

    [flyway] Fix wrong ddl

--- src/main/resources/db/sql/V1.0.21__add_table_book_log.sql
+++ src/main/resources/db/sql/V1.0.21__add_table_book_log.sql
@@ -1,19 +1,16 @@
 drop table if exists book_log;
 create table book_log
 (
-    id bigint not null auto_increment primary key,
-    title varchar(255) not null,
-    authors varchar(255) not null,
-    content varchar(1000),
-    isbn varchar(20) not null,
-    price int,
-    published_date date not null,
-    publisher varchar(255) not null,
-    thumbnail varchar(255)
+    id          bigint not null auto_increment primary key,
+    book_status varchar(255),
+    end_date    date,
+    start_date  date,
+    book_id     bigint,
+    member_id   bigint
 );
 
-alter table book_log add column book_id bigint not null;
-alter table book_log add constraint book_log_book_fk foreign key (book_id) references book (id);
+alter table book_log
+    add constraint book_log_book_fk foreign key (book_id) references book (id);
 
-alter table book_log add column member_id bigint not null;
-alter table book_log add constraint book_log_member_fk foreign key (member_id) references member (id);
+alter table book_log
+    add constraint book_log_member_fk foreign key (member_id) references member (id);
