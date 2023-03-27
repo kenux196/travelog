470181e44cfb0c1da33955e02d5422a822f98974
kenux196 <kenux.yun@gmail.com>
2023-03-27 16:36:00 +0900

    [FE] modify landing page

--- frontend-vue3-vite/src/components/landing/LandingBook.vue
+++ frontend-vue3-vite/src/components/landing/LandingBook.vue
@@ -31,10 +31,24 @@
         />
       </svg>
     </div>
-    <img
-      class="w-full"
-      src="https://images.unsplash.com/photo-1519682577862-22b62b24e493?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8JUVCJThGJTg1JUVDJTg0JTlDfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-    />
+    <div
+      class="hero min-h-screen"
+      style="
+        background-image: url(https://images.unsplash.com/photo-1519682577862-22b62b24e493?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8JUVCJThGJTg1JUVDJTg0JTlDfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60);
+      "
+    >
+      <div class="hero-overlay bg-opacity-60"></div>
+      <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+          <p class="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+            deleniti eaque aut repudiandae et a id nisi.
+          </p>
+          <button class="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
