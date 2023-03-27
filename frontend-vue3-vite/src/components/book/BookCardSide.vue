9ba456040ba7cac95a9386cd846ab4f7f1b3b8db
kenux196 <kenux.yun@gmail.com>
2023-03-27 21:55:37 +0900

    [FE] modify add book event

--- frontend-vue3-vite/src/components/book/BookCardSide.vue
+++ frontend-vue3-vite/src/components/book/BookCardSide.vue
@@ -6,21 +6,41 @@
       <p class="text-left text-xs">{{ book.authors }}</p>
       <p class="text-left text-xs">{{ book.publisher }}</p>
       <p class="text-left text-xs">{{ getPublishDate(book.publishedDate) }}</p>
-
     </div>
     <div class="card-actions">
-      <button class="btn btn-primary btn-sm">담기</button>
+      <button class="btn btn-primary btn-sm" @click="registerBook">담기</button>
     </div>
   </div>
 </template>
 
 <script setup>
-defineProps({
+import { request } from '@/api';
+const props = defineProps({
   book: Object,
 });
 defineEmits(['select:book']);
 
 const getPublishDate = (datetime) => {
   return new Date(datetime).toLocaleDateString();
+};
+
+// 책 등록 api 호출.
+const registerBook = async () => {
+  const bookInfos = [props.book.id];
+  console.log(bookInfos);
+  await request(
+    'post',
+    '/api/book-logs',
+    {
+      bookIds: bookInfos,
+    },
+    null,
+  )
+    .then(() => {
+      alert('나의 책장에 담았습니다.');
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
 };
 </script>
