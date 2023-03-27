545642b78efce636bf595393b3cc339e8263b62c
kenux196 <kenux.yun@gmail.com>
2023-03-27 10:45:46 +0900

    [FE] apply daisyui themes

--- frontend-vue3-vite/src/views/learn/DeaguEvent.vue
+++ frontend-vue3-vite/src/views/learn/DeaguEvent.vue
@@ -1,51 +1,42 @@
 <template>
-  <div class="text-center mx-10 text-slate-800">
-    <h1 class="font-bold text-lg mb-3">대구시 문화/공연/전시 행사 정보 검색</h1>
-    <hr class="my-3" />
-    <div class="flex">
-      <div class="">
-        <label for="selector"
-          >행사종류
-          <select id="selector" v-model="eventType" class="border border-slate-800">
-            <option v-for="item in eventTypeList" :key="item.id" :value="item.value">{{ item.name }}</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        <label
-          >시작일
-          <input type="date" v-model="startDate" />
-        </label>
-      </div>
-      <div>
-        <label
-          >종료일
-          <input type="date" v-model="endDate" />
-        </label>
-      </div>
-      <div>
-        <button class="ml-3" @click="searchEvent">검색</button>
-      </div>
-    </div>
+  <div class="text-center">
+    <h1 class="font-bold text-2xl my-3">대구시 문화/공연/전시 행사 정보 검색</h1>
   </div>
-  <hr class="mx-10 my-5" />
-  <div class="text-center mx-10">
+  <div class="flex justify-end">
+    <label for="selector"
+      >행사종류
+      <select id="selector" v-model="eventType" class="select bg-slate-800">
+        <option v-for="item in eventTypeList" :key="item.id" :value="item.value">{{ item.name }}</option>
+      </select>
+    </label>
+    <label
+      >시작일
+      <input type="date" v-model="startDate" :min="today" class="input bg-slate-800" />
+    </label>
+    <label
+      >종료일
+      <input type="date" v-model="endDate" class="input bg-slate-800" />
+    </label>
+    <button class="ml-3 btn" @click="searchEvent">검색</button>
+  </div>
+  <hr class="my-5" />
+  <div class="overflow-x-auto">
     <div v-if="hasEvent">
-      <table>
-        <thead class="bg-slate-600 text-slate-100">
-          <th class="w-10">종류</th>
+      <table class="table table-compact w-full">
+        <thead class="bg-slate-600 text-slate-100 text-center">
+          <th>종류</th>
           <th>주제</th>
           <th>장소</th>
-          <th class="w-20">유료/무료</th>
+          <th>유료/무료</th>
           <th>기간</th>
         </thead>
         <tbody>
           <tr v-for="event in eventList" :key="event.id" class="border-b-2 border-slate-800">
-            <td>{{ event.event_gubun_name }}</td>
-            <td class="pl-3 text-left">{{ event.subject }}</td>
-            <td>{{ event.place }}</td>
-            <td>{{ event.pay_gubun_name }}</td>
-            <td>{{ event.start_date + ' ~ ' + event.end_date }}</td>
+            <td class="text-center">{{ event.event_gubun_name }}</td>
+            <td class="text-left">{{ event.subject }}</td>
+            <td class="text-center">{{ event.place }}</td>
+            <td class="text-center">{{ event.pay_gubun_name }}</td>
+            <td class="text-center">{{ event.start_date + ' ~ ' + event.end_date }}</td>
           </tr>
         </tbody>
       </table>
@@ -56,9 +47,10 @@
 
 <script setup>
 import axios from 'axios';
-import { computed, ref } from 'vue';
+import { computed, ref, inject } from 'vue';
 
-const startDate = ref(new Date().toLocaleDateString());
+const today = inject('today');
+const startDate = ref(today);
 const endDate = ref('');
 const eventList = ref('');
 const eventType = ref('');
