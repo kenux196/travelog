5ef4ad467a124188534f66a1a20788012cc7127b
kenux196 <kenux.yun@gmail.com>
2023-04-01 10:28:57 +0900

    Add: find BookReview by book id

--- src/test/java/me/kenux/travelog/BaseRepositoryConfig.java
+++ src/test/java/me/kenux/travelog/BaseRepositoryConfig.java
@@ -1,6 +1,8 @@
 package me.kenux.travelog;
 
+import me.kenux.travelog.domain.member.repository.MemberRepository;
 import me.kenux.travelog.global.config.QueryDslConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
--- src/test/java/me/kenux/travelog/domain/booklog/repository/BookReviewRepositoryTest.java
+++ src/test/java/me/kenux/travelog/domain/booklog/repository/BookReviewRepositoryTest.java
@@ -6,18 +6,20 @@ import me.kenux.travelog.domain.booklog.entity.BookReview;
 import me.kenux.travelog.domain.member.entity.Member;
 import me.kenux.travelog.domain.member.entity.UserPassword;
 import me.kenux.travelog.domain.member.repository.MemberRepository;
+import me.kenux.travelog.domain.member.repository.PasswordRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
 class BookReviewRepositoryTest extends BaseRepositoryConfig {
 
     @Autowired
@@ -26,25 +28,43 @@ class BookReviewRepositoryTest extends BaseRepositoryConfig {
     private BookRepository bookRepository;
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private PasswordRepository passwordRepository;
+
+    private Book book;
+    private Member member;
 
     @BeforeEach
     void setup() {
-        LocalDate date = LocalDate.of(2023, 3, 30);
-        final Book book = Book.createNewBook("title", "author", "isbn", date, "publisher");
-        bookRepository.save(book);
-        final Member member = Member.createNewMember("member1", "member1@email.com", new UserPassword("password"));
-        memberRepository.save(member);
+        saveTestMemberData();
+        saveTestBookData();
         final BookReview review = BookReview.createBookReview(book, member, "review", 3);
         bookReviewRepository.save(review);
     }
 
+    private void saveTestMemberData() {
+        final UserPassword password = new UserPassword("password");
+        passwordRepository.save(password);
+        member = Member.createNewMember("member1", "member1@email.com", password);
+        memberRepository.save(member);
+    }
+
+    private void saveTestBookData() {
+        LocalDate date = LocalDate.of(2023, 3, 30);
+        book = Book.createNewBook("title", "author", "isbn", date, "publisher");
+        bookRepository.save(book);
+    }
+
     @Test
+    @DisplayName("find book_review data by book_id")
     void findByBookId() {
         // given
-
+        final Long bookId = book.getId();
 
         // when
+        List<BookReview> result = bookReviewRepository.findByBookId(bookId);
 
         // then
+        assertThat(result).hasSize(1);
     }
 }
