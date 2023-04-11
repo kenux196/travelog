6fa272a1f4c38e5c8fd5f18105e9c2a6f3c482de
kenux196 <kenux.yun@gmail.com>
2023-04-11 22:01:52 +0900

    Add BookLog new logic

---
+++ src/main/java/me/kenux/travelog/domain/booklog/service/dto/AddBookLogRequestWithBookInfo.java
@@ -0,0 +1,9 @@
+package me.kenux.travelog.domain.booklog.service.dto;
+
+import lombok.Data;
+
+@Data
+public class AddBookLogRequestWithBookInfo {
+
+    private BookInfoDto bookInfo;
+}
--- src/test/java/me/kenux/travelog/domain/booklog/service/BookLogServiceTest.java
+++ src/test/java/me/kenux/travelog/domain/booklog/service/BookLogServiceTest.java
@@ -5,12 +5,15 @@ import me.kenux.travelog.domain.booklog.entity.BookLog;
 import me.kenux.travelog.domain.booklog.repository.BookLogRepository;
 import me.kenux.travelog.domain.booklog.repository.BookRepository;
 import me.kenux.travelog.domain.booklog.service.dto.AddBookLogRequest;
+import me.kenux.travelog.domain.booklog.service.dto.AddBookLogRequestWithBookInfo;
+import me.kenux.travelog.domain.booklog.service.dto.BookInfoDto;
 import me.kenux.travelog.domain.booklog.service.dto.BookLogResponse;
 import me.kenux.travelog.domain.member.entity.Member;
 import me.kenux.travelog.domain.member.repository.MemberRepository;
 import me.kenux.travelog.global.exception.CustomException;
 import me.kenux.travelog.global.exception.ErrorCode;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,6 +26,7 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -31,7 +35,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,11 +51,14 @@ class BookLogServiceTest {
     @InjectMocks
     private BookLogService bookLogService;
 
+    private Member mockMember;
+    private Book mockBook;
+
     private static SecurityContext securityContext;
     private static Authentication authentication;
 
     @BeforeAll
-    static void setup() {
+    static void setupAll() {
         authentication = Mockito.mock(Authentication.class);
         securityContext = getMockSecurityContext();
 
@@ -62,11 +69,19 @@ class BookLogServiceTest {
         return securityContext;
     }
 
+    @BeforeEach
+    void setup() {
+        mockMember = mock(Member.class);
+        mockBook = mock(Book.class);
+        given(securityContext.getAuthentication()).willReturn(authentication);
+    }
+
     @Test
     @DisplayName("회원의 북로그 정보를 가져온다. - 성공")
     void getBookLogForMember() {
         // given
-        given(bookLogRepository.findByMember(any())).willReturn(getDummyBookLogs());
+        given(bookLogRepository.findByMember(any()))
+                .willReturn(Collections.singletonList(BookLog.createNewLog(mockBook, mockMember)));
 
         // when
         final List<BookLogResponse> bookLogResponses = bookLogService.getBookLogs(any());
@@ -75,20 +90,13 @@ class BookLogServiceTest {
         assertThat(bookLogResponses).isNotEmpty();
     }
 
-    private List<BookLog> getDummyBookLogs() {
-        final Book mockBook = Mockito.mock(Book.class);
-        final Member mockMember = Mockito.mock(Member.class);
-        return Collections.singletonList(BookLog.createNewLog(mockBook, mockMember));
-    }
-
     @Test
     @DisplayName("새로운 북로그를 추가 실패 - 책이 존재하지 않음")
     void addBookLog_failed_bookNotFounded() {
         // given
         AddBookLogRequest request = getAddBookLogRequest();
-        given(securityContext.getAuthentication()).willReturn(authentication);
-        given(memberRepository.findByEmail(any())).willReturn(Optional.of(mock(Member.class)));
         given(bookRepository.findAllById(any())).willReturn(new ArrayList<>());
+        setStubMember();
 
         // when
         final Throwable throwable = catchThrowable(() -> bookLogService.addNewBookLog(request));
@@ -120,10 +128,8 @@ class BookLogServiceTest {
     void addBookLog_failed_success() {
         // given
         AddBookLogRequest request = getAddBookLogRequest();
-        final Book book = Book.createNewBook("book1", "author", "isbn", LocalDate.now(), "publisher");
-        given(bookRepository.findAllById(any())).willReturn(Collections.singletonList(book));
-        given(securityContext.getAuthentication()).willReturn(authentication);
-        given(memberRepository.findByEmail(any())).willReturn(Optional.of(Mockito.mock(Member.class)));
+        given(bookRepository.findAllById(any())).willReturn(Collections.singletonList(mockBook));
+        setStubMember();
 
         // when
         final int addedBookCount = bookLogService.addNewBookLog(request);
@@ -132,10 +138,56 @@ class BookLogServiceTest {
         assertThat(addedBookCount).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("책정보를 기준으로 새로운 북로그 생성 - 기존에 등록된 책이면 바로 북로그 생성 - 성공")
+    void addBookLog_withBookInfo_success() {
+        // given
+        AddBookLogRequestWithBookInfo request = Mockito.mock(AddBookLogRequestWithBookInfo.class);
+        BookInfoDto bookInfoDto = mock(BookInfoDto.class);
+        given(request.getBookInfo()).willReturn(bookInfoDto);
+        given(bookRepository.findBookByIsbn(any())).willReturn(Optional.of(mockBook));
+        setStubMember();
+
+        // when
+        final Long result = bookLogService.addNewBookLogWithBookInfo(request);
+
+        // then
+        then(bookLogRepository).should(times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("책정보를 기준으로 새로운 북로그 생성 - 기존에 등록되지 않은 책은 새로 책 등록 후 북로그 생성 성공")
+    void addBookLog_withBookInfo_notExistBook_success() {
+        // given
+        AddBookLogRequestWithBookInfo request = Mockito.mock(AddBookLogRequestWithBookInfo.class);
+        BookInfoDto bookInfoDto = BookInfoDto.builder()
+                .title("title")
+                .publisher("publisher")
+                .authors("author")
+                .isbn("isbn")
+                .datetime(OffsetDateTime.now())
+                .build();
+        given(request.getBookInfo()).willReturn(bookInfoDto);
+        given(bookRepository.findBookByIsbn(any())).willReturn(Optional.empty());
+        given(bookRepository.save(any())).willReturn(mockBook);
+        setStubMember();
+
+        // when
+        final Long result = bookLogService.addNewBookLogWithBookInfo(request);
+
+        // then
+        then(bookRepository).should(times(1)).save(any());
+        then(bookLogRepository).should(times(1)).save(any());
+    }
+
     private AddBookLogRequest getAddBookLogRequest() {
         AddBookLogRequest request = new AddBookLogRequest();
         List<Long> bookIds = Collections.singletonList(1L);
         request.setBookIds(bookIds);
         return request;
+    }
+
+    private void setStubMember() {
+        given(memberRepository.findByEmail(any())).willReturn(Optional.of(mockMember));
     }
 }
