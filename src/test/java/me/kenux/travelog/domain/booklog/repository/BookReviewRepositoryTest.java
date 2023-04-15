53af9514a970547d4db1c6c11de0d72f88bf7c5c
kenux196 <kenux.yun@gmail.com>
2023-04-15 13:17:35 +0900

    Modify: add @parameterizedTest

--- src/test/java/me/kenux/travelog/domain/booklog/repository/BookReviewRepositoryTest.java
+++ src/test/java/me/kenux/travelog/domain/booklog/repository/BookReviewRepositoryTest.java
@@ -8,21 +8,23 @@ import me.kenux.travelog.domain.member.entity.Member;
 import me.kenux.travelog.domain.member.entity.UserPassword;
 import me.kenux.travelog.domain.member.repository.MemberRepository;
 import me.kenux.travelog.domain.member.repository.PasswordRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BookReviewRepositoryTest extends BaseRepositoryConfig {
 
-    @Autowired
-    private TestEntityManager em;
     @Autowired
     private BookReviewRepository bookReviewRepository;
     @Autowired
@@ -32,85 +34,15 @@ class BookReviewRepositoryTest extends BaseRepositoryConfig {
     @Autowired
     private PasswordRepository passwordRepository;
 
-    private Book book;
     private Member member;
+    private Book book;
 
-    @BeforeEach
+    @BeforeAll
     void setup() {
         saveTestMemberData();
         saveTestBookData();
         final BookReview review = BookReview.createBookReview(book, member, "review", 3);
         bookReviewRepository.save(review);
-        em.flush();
-        em.clear();
-    }
-
-    @Test
-    @DisplayName("find book review by bookId")
-    void findByBookId() {
-        // given
-        final Long bookId = book.getId();
-
-        // when
-        List<BookReview> result = bookReviewRepository.findByBookId(bookId);
-
-        // then
-        assertThat(result).hasSize(1);
-    }
-
-    @Test
-    @DisplayName("find book review by memberId")
-    void findByMemberId() {
-        // given
-        final Long memberId = member.getId();
-
-        // when
-        final List<BookReview> result = bookReviewRepository.findByMemberId(memberId);
-
-        // then
-        assertThat(result).hasSize(1);
-    }
-
-    @Test
-    @DisplayName("find book review by search condition - memberId")
-    void findReviewByCondition_conditionHasMemberId_success() {
-        // given
-        BookReviewSearchCond cond = new BookReviewSearchCond();
-        cond.setMemberId(member.getId());
-
-        // when
-        final List<BookReview> reviews = bookReviewRepository.findReviewByCondition(cond);
-
-        // then
-        assertThat(reviews).hasSize(1);
-    }
-
-    @Test
-    @DisplayName("find book review by search condition - bookId")
-    void findReviewByCondition_conditionHasBookId_success() {
-        // given
-        BookReviewSearchCond cond = new BookReviewSearchCond();
-        cond.setBookId(book.getId());
-
-        // when
-        final List<BookReview> reviews = bookReviewRepository.findReviewByCondition(cond);
-
-        // then
-        assertThat(reviews).hasSize(1);
-    }
-
-    @Test
-    @DisplayName("find book review with member data")
-    void findReviewByBook() {
-        // given
-        final Long bookId = book.getId();
-
-        // when
-        final List<BookReview> reviews = bookReviewRepository.findReviewWithMemberBy(bookId);
-
-        // then
-        assertThat(reviews).hasSize(1);
-        assertThat(reviews.get(0).getMember()).isInstanceOf(Member.class);
     }
 
     private void saveTestMemberData() {
@@ -124,5 +56,61 @@ class BookReviewRepositoryTest extends BaseRepositoryConfig {
         LocalDate date = LocalDate.of(2023, 3, 30);
         book = Book.createNewBook("title", "author", "isbn", date, "publisher");
         bookRepository.save(book);
+    }
+
+    @Test
+    @DisplayName("find book review by bookId")
+    void findByBookId() {
+        // when
+        List<BookReview> result = bookReviewRepository.findByBookId(book.getId());
+
+        // then
+        assertThat(result).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("find book review by memberId")
+    void findByMemberId() {
+        // when
+        final List<BookReview> result = bookReviewRepository.findByMemberId(member.getId());
+
+        // then
+        assertThat(result).hasSize(1);
+    }
+
+    @ParameterizedTest(name = "{index}: {2}")
+    @MethodSource("provideBookReviewSearchCond")
+    @DisplayName("find book review by search condition")
+    void findReviewByCondition(Long memberId, Long bookId, String message, int count) {
+        System.out.println("memberId = " + memberId + " bookId = " + bookId);
+        // given
+        final BookReviewSearchCond cond = new BookReviewSearchCond(memberId, bookId);
+
+        // when
+        final List<BookReview> reviews = bookReviewRepository.findReviewByCondition(cond);
+
+        // then
+        assertThat(reviews).hasSize(count);
+    }
+
+    private static Stream<Arguments> provideBookReviewSearchCond() {
+        System.out.println("provideBookReviewSearchCond() = ");
+        return Stream.of(
+                Arguments.of(null, null, "no condition - find all", 1),
+                Arguments.of(1L, null, "find by memberId", 1),
+                Arguments.of(null, 1L, "find by bookId", 1),
+                Arguments.of(1L, 1L, "find by both(member, book)", 1)
+        );
+    }
+
+    @Test
+    @DisplayName("find book review with member data")
+    void findReviewByBook() {
+        // when
+        final List<BookReview> reviews = bookReviewRepository.findReviewWithMemberBy(book.getId());
+
+        // then
+        assertThat(reviews).hasSize(1);
+        assertThat(reviews.get(0).getMember()).isInstanceOf(Member.class);
     }
 }
