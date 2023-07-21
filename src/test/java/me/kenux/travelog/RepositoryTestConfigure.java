0daf7c48522a1c9657a6b2733505cdfeae52bb58
kenux196 <kenux.yun@gmail.com>
2023-07-22 00:23:27 +0900

    Add application-test.yml

--- src/test/java/me/kenux/travelog/RepositoryTestConfigure.java
+++ src/test/java/me/kenux/travelog/RepositoryTestConfigure.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 @Import(QueryDslConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RepositoryTestConfigure {
