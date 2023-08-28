e1dd57ab9976f25027ae845bd4361ce6c59636c1
kenux196 <kenux.yun@gmail.com>
2023-08-28 22:07:04 +0900

    add swagger

--- src/main/java/me/kenux/travelog/web/api/HelloRestController.java
+++ src/main/java/me/kenux/travelog/web/api/HelloRestController.java
@@ -1,27 +1,37 @@
 package me.kenux.travelog.web.api;
 
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "Hello Swagger")
 @RestController
 @RequestMapping("/hello")
 @Slf4j
 public class HelloRestController {
 
+    @ApiOperation(value = "Hello: GET")
     @GetMapping
     public ResponseEntity<?> getHello() {
+
         log.info("GET: /hello");
         return ResponseEntity.ok("OK~~~");
     }
 
-    @PostMapping
-    public ResponseEntity<?> postHello() {
+    @ApiOperation(value = "Hello 이름 등록")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "등록 성공"),
+            @ApiResponse(code = 500, message = "서버 에러")
+    })
+    @PostMapping("/{name}")
+    public ResponseEntity<?> postHello(@ApiParam(value = "name") @PathVariable String name) {
         log.info("POST: /hello");
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        if (name.equals("admin")) {
+            throw new IllegalArgumentException("유효하지 않은 이름입니다.");
+        }
+        return ResponseEntity.ok(name + "이 등록되었습니다.");
     }
 }
