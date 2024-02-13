09fa6712a30c285a31a9f3e1141c9d369cf5180e
kenux196 <kenux.yun@gmail.com>
2024-02-13 17:42:34 +0900

    [swagger] add @Operation and @ApiResponse sample

--- src/main/java/me/kenux/travelog/domain/member/service/dto/response/MemberInfo.java
+++ src/main/java/me/kenux/travelog/domain/member/service/dto/response/MemberInfo.java
@@ -1,21 +1,25 @@
 package me.kenux.travelog.domain.member.service.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import me.kenux.travelog.domain.member.entity.Member;
 
 import java.time.OffsetDateTime;
 
+@Schema(description = "회원 정보 응답 메시지")
 @Data
 @Builder
 public class MemberInfo {
 
+    @Schema(description = "심플 회원 정보 응답 메시지")
     public record SimpleResponse(Long id, String name, String email, OffsetDateTime joinDate) {
         public static SimpleResponse of(Member member) {
             return new SimpleResponse(member.getId(), member.getName(), member.getEmail(), member.getCreatedDate());
         }
     }
 
+    @Schema(description = "상세 회원 정보 응답 메시지")
     @Builder
     public record DetailResponse(Long id, String name, String email, OffsetDateTime joinDate, String status, String role) {
 
--- src/main/java/me/kenux/travelog/web/api/member/MemberController.java
+++ src/main/java/me/kenux/travelog/web/api/member/MemberController.java
@@ -1,5 +1,9 @@
 package me.kenux.travelog.web.api.member;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import me.kenux.travelog.domain.member.service.MemberService;
@@ -19,6 +23,9 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "회원 정보 조회", description = "회원 번호(id)를 이용하여 회원을 조회한다.")
+    @ApiResponse(responseCode = "200", description = "회원 조회 성공",
+            content = @Content(schema = @Schema(implementation = MemberInfo.SimpleResponse.class)))
     public ResponseEntity<?> getMyInfo(@PathVariable("id") Long id) {
         final MemberInfo.SimpleResponse memberInfo = memberService.getMemberSimpleInfo(id);
         return ResponseEntity.ok(memberInfo);
