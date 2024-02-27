6382886ee824245a3fe132584d785a13057c5e72
kenux196 <kenux.yun@gmail.com>
2024-02-27 10:42:40 +0900

    update vue 3.4.x

--- frontend-vue3-vite/src/router/index.js
+++ frontend-vue3-vite/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore } from './../stores/auth';
 import HomeView from '../views/HomeView.vue';
-import LoginView from '../views/Auth/LoginView.vue';
+import LoginView from '../views/auth/LoginView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +17,7 @@ const router = createRouter({
     },
     {
       path: '/signup',
-      component: () => import('../views/Auth/SignupView.vue'),
+      component: () => import('../views/auth/SignupView.vue'),
     },
     {
       path: '/admin',
